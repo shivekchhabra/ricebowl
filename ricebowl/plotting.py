24f0d9ef460eb694b16ad0e72503249d1ce7ac32
Shivek <shivekchhabra@gmail.com>
2020-08-27 19:13:53 +0530

    [1.4.0] Added plotting facility

---
+++ ricebowl/plotting.py
@@ -0,0 +1,26 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+# General function to get a pair plot of the entire data. Hue can be modified.
+def pairplot(df, hue=None, cols=['ALL']):
+    if cols == ['ALL']:
+        sns.pairplot(data=df, hue=hue)
+    else:
+        sns.pairplot(data=df, hue=hue, vars=cols)
+    plt.show()
+
+
+# General function to get plots for all columns passed. Press 'q' for next figure.
+def distribution(df, **col_names):
+    for i in col_names.values():
+        sns.distplot(df[i], label=i)
+        plt.show()
+
+
+# General function to plot relationship between 2 random variables. x,y have input types as list/df series.
+def plot(x, y, xlabel='x', ylabel='y'):
+    plt.plot(x, 'g*', y, 'ro')
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.show()
